bdde6853d73fb0fb4daeedce6665e3c6d3111af0
Mike Hartington <mikehartington@gmail.com>
2019-03-11 11:30:31 -0400

    add

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -8,50 +8,30 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'sessions',
         children: [
           {
             path: '',
-            loadChildren: '../tab1/tab1.module#Tab1PageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab2',
-        children: [
+            loadChildren: '../sessions/sessions.module#SessionsPageModule'
+          },
           {
-            path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
+            path: ':id',
+            loadChildren:
+              '../session-detail/session-detail.module#SessionDetailPageModule'
           }
         ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab3/tab3.module#Tab3PageModule'
-          }
-        ]
-      },
-      {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/sessions',
     pathMatch: 'full'
   }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes)
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class TabsPageRoutingModule {}
